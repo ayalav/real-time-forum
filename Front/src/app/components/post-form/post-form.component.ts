eab269ced54fa9b97612e9d013829581c654e8a8
ayalav <apap90948@gmail.com>
2024-10-13 02:58:29 +0300

    postFormComponent extraction fix

--- Front/src/app/components/post-form/post-form.component.ts
+++ Front/src/app/components/post-form/post-form.component.ts
@@ -1,33 +1,40 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EditorComponent } from '../editor/editor.component';
 
 @Component({
   selector: 'app-post-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    EditorComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    ],
   templateUrl: './post-form.component.html',
   styleUrl: './post-form.component.scss'
 })
 export class PostFormComponent {
-  @Input() postForm!: FormGroup;
+  @Input() postForm!: FormGroup;  
   @Input() showPostForm!: boolean;
-  @Output() toggleForm = new EventEmitter<void>();
-  @Output() submitPost = new EventEmitter<void>();
+  @Output() toggleForm = new EventEmitter<void>(); 
+  @Output() submitPost = new EventEmitter<void>(); 
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
-  // Getter for content FormControl
-  getContent() {
-    return this.postForm.get('content');
-  }
-
-  onSubmit() {
+  getContent(): FormControl {
+    return this.postForm.get('content') as FormControl;
+}
+  createPost() {
     if (this.postForm.valid) {
-      this.submitPost.emit();
+      this.submitPost.emit(); 
     }
   }
 
   onCancel() {
-    this.toggleForm.emit();
+    this.toggleForm.emit(); 
   }
 }
