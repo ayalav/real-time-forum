63c7efe04e9c5377d6af127e38eb5c802a5e1068
ayalav <apap90948@gmail.com>
2024-10-13 12:39:36 +0300

    Improve post and comment display with author information

--- Front/src/app/models/comment.ts
+++ Front/src/app/models/comment.ts
@@ -1,5 +1,6 @@
   export interface Comment {
     id?: number;
     content: string;
+    userName?: string;
   }
   
--- Front/src/app/models/post.ts
+++ Front/src/app/models/post.ts
@@ -2,6 +2,7 @@ export interface Post {
     id: number;
     title: string;
     content: string;
+    userName?: string;
     showComments?: boolean;
     comments: Comment[];
   }
